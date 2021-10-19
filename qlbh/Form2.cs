f7de13ebdb9c12c8c82b9f8eba5b1d378431ff7a
JoyceVuVu <vuvu7764111@gmail.com>
2021-10-19 21:56:13 +0700

    submit sqlconnection

---
+++ qlbh/Form2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace qlbh
+{
+    public partial class Form2 : Form
+    {
+        SQLConnection kn = new SQLConnection();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void BANG_nhanvien()//tạo thủ tục để gọi nhiều lần
+        {
+            DataTable dta = new DataTable();
+            dta = kn.Lay_DulieuBang("Select * from nhanvien");
+            dataGridView1.DataSource = dta;
+            //HIENTHI_DULIEU();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            BANG_nhanvien();
+        }
+    }
+}
---
+++ qlbh/SQLConnection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace qlbh
+{
+    //Data Source=DESKTOP-1AMUFBN\SQLEXPRESS;Initial Catalog=qlbanhang;Integrated Security=True
+    class SQLConnection
+    {
+        public SqlConnection cnn;
+        public SqlCommand cmd;
+        public DataTable dta;
+        public SqlDataAdapter ada;
+
+       
+        public void Ketnoi_Dulieu()
+        {
+            string Ketnoi = @"Data Source=DESKTOP-1AMUFBN\SQLEXPRESS;Initial Catalog=qlbanhang;Integrated Security=True";
+            cnn = new SqlConnection(Ketnoi);
+            cnn.open();
+        }
+        public void HuyKetnoi()
+        {
+            if (cnn.State == ConnectionState.Open)
+                cnn.Close();
+        }
+        public DataTable Lay_Dulieubang(string sql)
+        {
+            Ketnoi_Dulieu();
+            ada = new SqlDataAdapter(sql, cnn);
+            dta = new DataTable();
+            ada.Fill(dta);
+            return dta;
+        }
+        public void ThucThi(string sql)
+        {
+            Ketnoi_Dulieu();
+            cmd = new SqlCommand(sql,cnn);
+            cmd.ExcuteNonQuery();
+            HuyKetnoi();
+        }
+    }
+}
