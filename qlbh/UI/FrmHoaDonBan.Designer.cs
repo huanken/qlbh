5d1f72e4c741dbf23e57274c5e760a3f25a242c7
huanken <koon2105@gmail.com>
2021-10-23 14:43:29 +0700

    forms

--- qlbh/UI/FrmHoaDonBan.Designer.cs
+++ qlbh/UI/FrmHoaDonBan.Designer.cs
@@ -29,10 +29,18 @@ namespace qlbh.UI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmHoaDonBan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(910, 510);
+            this.ControlBox = false;
+            this.Name = "FrmHoaDonBan";
             this.Text = "FrmHoaDonBan";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- qlbh/UI/FrmNhanVien.Designer.cs
+++ qlbh/UI/FrmNhanVien.Designer.cs
@@ -29,10 +29,18 @@ namespace qlbh.UI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // FrmNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(863, 503);
+            this.ControlBox = false;
+            this.Name = "FrmNhanVien";
             this.Text = "FrmNhanVien";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
