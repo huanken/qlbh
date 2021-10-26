adad9396e24172898bfd53582ab9d1861bbe4cc2
Thuha3011 <phamthuha30112000gmail.com>
2021-10-26 15:41:45 +0700

    add form nhanvien

--- qlbh/UI/FrmNhaCungCap.Designer.cs
+++ qlbh/UI/FrmNhaCungCap.Designer.cs
@@ -41,10 +41,10 @@ namespace qlbh.UI
             this.btnxoa = new CustomControls.RJControls.RJButton();
             this.btnThoat = new CustomControls.RJControls.RJButton();
             this.dataGridViewncc = new System.Windows.Forms.DataGridView();
-            this.txtmancc = new CustomControls.RJControls.RJTextBox();
-            this.txttenncc = new CustomControls.RJControls.RJTextBox();
-            this.txtdiachi = new CustomControls.RJControls.RJTextBox();
-            this.txtsodt = new CustomControls.RJControls.RJTextBox();
+            this.rjTextBox1 = new CustomControls.RJControls.RJTextBox();
+            this.rjTextBox2 = new CustomControls.RJControls.RJTextBox();
+            this.rjTextBox3 = new CustomControls.RJControls.RJTextBox();
+            this.rjTextBox4 = new CustomControls.RJControls.RJTextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewncc)).BeginInit();
             this.SuspendLayout();
@@ -228,93 +228,93 @@ namespace qlbh.UI
             this.dataGridViewncc.TabIndex = 4;
             this.dataGridViewncc.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewncc_CellContentClick);
             // 
-            // txtmancc
+            // rjTextBox1
             // 
-            this.txtmancc.BackColor = System.Drawing.SystemColors.Window;
-            this.txtmancc.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtmancc.BorderFocusColor = System.Drawing.Color.HotPink;
-            this.txtmancc.BorderRadius = 10;
-            this.txtmancc.BorderSize = 2;
-            this.txtmancc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtmancc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtmancc.Location = new System.Drawing.Point(225, 98);
-            this.txtmancc.Margin = new System.Windows.Forms.Padding(4);
-            this.txtmancc.Multiline = false;
-            this.txtmancc.Name = "txtmancc";
-            this.txtmancc.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
-            this.txtmancc.PasswordChar = false;
-            this.txtmancc.PlaceholderColor = System.Drawing.Color.DarkGray;
-            this.txtmancc.PlaceholderText = "";
-            this.txtmancc.Size = new System.Drawing.Size(202, 31);
-            this.txtmancc.TabIndex = 5;
-            this.txtmancc.Texts = "";
-            this.txtmancc.UnderlinedStyle = false;
+            this.rjTextBox1.BackColor = System.Drawing.SystemColors.Window;
+            this.rjTextBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rjTextBox1.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.rjTextBox1.BorderRadius = 10;
+            this.rjTextBox1.BorderSize = 2;
+            this.rjTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjTextBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.rjTextBox1.Location = new System.Drawing.Point(225, 98);
+            this.rjTextBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.rjTextBox1.Multiline = false;
+            this.rjTextBox1.Name = "rjTextBox1";
+            this.rjTextBox1.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.rjTextBox1.PasswordChar = false;
+            this.rjTextBox1.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.rjTextBox1.PlaceholderText = "";
+            this.rjTextBox1.Size = new System.Drawing.Size(202, 31);
+            this.rjTextBox1.TabIndex = 5;
+            this.rjTextBox1.Texts = "";
+            this.rjTextBox1.UnderlinedStyle = false;
             // 
-            // txttenncc
+            // rjTextBox2
             // 
-            this.txttenncc.BackColor = System.Drawing.SystemColors.Window;
-            this.txttenncc.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txttenncc.BorderFocusColor = System.Drawing.Color.HotPink;
-            this.txttenncc.BorderRadius = 10;
-            this.txttenncc.BorderSize = 2;
-            this.txttenncc.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txttenncc.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txttenncc.Location = new System.Drawing.Point(225, 137);
-            this.txttenncc.Margin = new System.Windows.Forms.Padding(4);
-            this.txttenncc.Multiline = false;
-            this.txttenncc.Name = "txttenncc";
-            this.txttenncc.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
-            this.txttenncc.PasswordChar = false;
-            this.txttenncc.PlaceholderColor = System.Drawing.Color.DarkGray;
-            this.txttenncc.PlaceholderText = "";
-            this.txttenncc.Size = new System.Drawing.Size(202, 31);
-            this.txttenncc.TabIndex = 5;
-            this.txttenncc.Texts = "";
-            this.txttenncc.UnderlinedStyle = false;
+            this.rjTextBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.rjTextBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rjTextBox2.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.rjTextBox2.BorderRadius = 10;
+            this.rjTextBox2.BorderSize = 2;
+            this.rjTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjTextBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.rjTextBox2.Location = new System.Drawing.Point(225, 137);
+            this.rjTextBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.rjTextBox2.Multiline = false;
+            this.rjTextBox2.Name = "rjTextBox2";
+            this.rjTextBox2.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.rjTextBox2.PasswordChar = false;
+            this.rjTextBox2.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.rjTextBox2.PlaceholderText = "";
+            this.rjTextBox2.Size = new System.Drawing.Size(202, 31);
+            this.rjTextBox2.TabIndex = 5;
+            this.rjTextBox2.Texts = "";
+            this.rjTextBox2.UnderlinedStyle = false;
             // 
-            // txtdiachi
+            // rjTextBox3
             // 
-            this.txtdiachi.BackColor = System.Drawing.SystemColors.Window;
-            this.txtdiachi.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtdiachi.BorderFocusColor = System.Drawing.Color.HotPink;
-            this.txtdiachi.BorderRadius = 10;
-            this.txtdiachi.BorderSize = 2;
-            this.txtdiachi.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtdiachi.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtdiachi.Location = new System.Drawing.Point(225, 171);
-            this.txtdiachi.Margin = new System.Windows.Forms.Padding(4);
-            this.txtdiachi.Multiline = false;
-            this.txtdiachi.Name = "txtdiachi";
-            this.txtdiachi.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
-            this.txtdiachi.PasswordChar = false;
-            this.txtdiachi.PlaceholderColor = System.Drawing.Color.DarkGray;
-            this.txtdiachi.PlaceholderText = "";
-            this.txtdiachi.Size = new System.Drawing.Size(202, 31);
-            this.txtdiachi.TabIndex = 5;
-            this.txtdiachi.Texts = "";
-            this.txtdiachi.UnderlinedStyle = false;
+            this.rjTextBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.rjTextBox3.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rjTextBox3.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.rjTextBox3.BorderRadius = 10;
+            this.rjTextBox3.BorderSize = 2;
+            this.rjTextBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjTextBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.rjTextBox3.Location = new System.Drawing.Point(225, 171);
+            this.rjTextBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.rjTextBox3.Multiline = false;
+            this.rjTextBox3.Name = "rjTextBox3";
+            this.rjTextBox3.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.rjTextBox3.PasswordChar = false;
+            this.rjTextBox3.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.rjTextBox3.PlaceholderText = "";
+            this.rjTextBox3.Size = new System.Drawing.Size(202, 31);
+            this.rjTextBox3.TabIndex = 5;
+            this.rjTextBox3.Texts = "";
+            this.rjTextBox3.UnderlinedStyle = false;
             // 
-            // txtsodt
+            // rjTextBox4
             // 
-            this.txtsodt.BackColor = System.Drawing.SystemColors.Window;
-            this.txtsodt.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
-            this.txtsodt.BorderFocusColor = System.Drawing.Color.HotPink;
-            this.txtsodt.BorderRadius = 10;
-            this.txtsodt.BorderSize = 2;
-            this.txtsodt.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtsodt.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtsodt.Location = new System.Drawing.Point(225, 205);
-            this.txtsodt.Margin = new System.Windows.Forms.Padding(4);
-            this.txtsodt.Multiline = false;
-            this.txtsodt.Name = "txtsodt";
-            this.txtsodt.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
-            this.txtsodt.PasswordChar = false;
-            this.txtsodt.PlaceholderColor = System.Drawing.Color.DarkGray;
-            this.txtsodt.PlaceholderText = "";
-            this.txtsodt.Size = new System.Drawing.Size(202, 31);
-            this.txtsodt.TabIndex = 5;
-            this.txtsodt.Texts = "";
-            this.txtsodt.UnderlinedStyle = false;
+            this.rjTextBox4.BackColor = System.Drawing.SystemColors.Window;
+            this.rjTextBox4.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.rjTextBox4.BorderFocusColor = System.Drawing.Color.HotPink;
+            this.rjTextBox4.BorderRadius = 10;
+            this.rjTextBox4.BorderSize = 2;
+            this.rjTextBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rjTextBox4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.rjTextBox4.Location = new System.Drawing.Point(225, 205);
+            this.rjTextBox4.Margin = new System.Windows.Forms.Padding(4);
+            this.rjTextBox4.Multiline = false;
+            this.rjTextBox4.Name = "rjTextBox4";
+            this.rjTextBox4.Padding = new System.Windows.Forms.Padding(10, 7, 10, 7);
+            this.rjTextBox4.PasswordChar = false;
+            this.rjTextBox4.PlaceholderColor = System.Drawing.Color.DarkGray;
+            this.rjTextBox4.PlaceholderText = "";
+            this.rjTextBox4.Size = new System.Drawing.Size(202, 31);
+            this.rjTextBox4.TabIndex = 5;
+            this.rjTextBox4.Texts = "";
+            this.rjTextBox4.UnderlinedStyle = false;
             // 
             // FrmNhaCungCap
             // 
@@ -322,10 +322,10 @@ namespace qlbh.UI
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(852, 508);
             this.ControlBox = false;
-            this.Controls.Add(this.txtsodt);
-            this.Controls.Add(this.txtdiachi);
-            this.Controls.Add(this.txttenncc);
-            this.Controls.Add(this.txtmancc);
+            this.Controls.Add(this.rjTextBox4);
+            this.Controls.Add(this.rjTextBox3);
+            this.Controls.Add(this.rjTextBox2);
+            this.Controls.Add(this.rjTextBox1);
             this.Controls.Add(this.dataGridViewncc);
             this.Controls.Add(this.btnThoat);
             this.Controls.Add(this.btnxoa);
@@ -364,9 +364,9 @@ namespace qlbh.UI
         private CustomControls.RJControls.RJButton btnxoa;
         private CustomControls.RJControls.RJButton btnThoat;
         private System.Windows.Forms.DataGridView dataGridViewncc;
-        private CustomControls.RJControls.RJTextBox txtmancc;
-        private CustomControls.RJControls.RJTextBox txttenncc;
-        private CustomControls.RJControls.RJTextBox txtdiachi;
-        private CustomControls.RJControls.RJTextBox txtsodt;
+        private CustomControls.RJControls.RJTextBox rjTextBox1;
+        private CustomControls.RJControls.RJTextBox rjTextBox2;
+        private CustomControls.RJControls.RJTextBox rjTextBox3;
+        private CustomControls.RJControls.RJTextBox rjTextBox4;
     }
 }
